b2eb0fc2075dc3aa5e0fab246d933530eb6fa836
Christoffer Heien <Chriss4302@hotmail.com>
2026-01-30 11:47:28 +0100

    Implement logger setup function

--- logger.py
+++ logger.py
@@ -1,1 +1,25 @@
+import logging
+
+
+def setup_logger(log_file="backup.log"):
+    logger = logging.getLogger("backup")
+    logger.setLevel(logging.INFO)
+
+    if logger.handlers:
+        return logger
+
+    formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(message)s"
+    )
+
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
 
